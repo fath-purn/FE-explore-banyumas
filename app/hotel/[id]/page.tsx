b0c64817620e22ea6131625b01abf2005e6a3134
fath-purn <talitakamilia345@gmail.com>
2023-11-14 16:59:38 +0700

    feat: detail hotel

--- app/hotel/[id]/page.tsx
+++ app/hotel/[id]/page.tsx
@@ -4,12 +4,13 @@ import Link from "next/link";
 import Keterangan from "@/app/ui/wisata/keterangan";
 import { mdiMapMarker } from "@mdi/js";
 import Icon from "@mdi/react";
-import CardHotel from "@/app/ui/hotel/cardHotel";
-import { dataHotel } from "@/app/page";
+import { dataWisata } from "@/app/page";
 import Footer from "@/app/ui/footer";
+import CardWisata from "@/app/ui/wisata/cardWisata";
+import Fasilitas from "@/app/ui/hotel/fasilitas";
 
 // panggil dari api
-export const dataWisataById = [
+export const dataHotelById = [
   {
     id: "0",
     nama: "Baturaden",
@@ -18,13 +19,9 @@ export const dataWisataById = [
       image2: "https://via.placeholder.com/574x187",
       image3: "https://via.placeholder.com/574x187",
     },
-    deskripsi: `Lokasi wisata Baturaden terletak di sebelah selatan lereng Gunung
-    Slamet, Kabupaten Banyumas, Jawa Tengah Disini wisatawan dapat
-    menikmati panorama Gunung Slamet, wisatawanpun akan betah
-    berlama-lama disini karena Lokawisata Baturraden menyediakan
-    berbagai fasilitas super lengkap`,
+    deskripsi: `Luminor Hotel Purwokerto telah beroperasi sejak tahun 2010 dan merupakan anak perusahaan dari Waringin Group. Luminor Hotel Purwokerto memiliki tiga (3) tipe kamar, Deluxe Room, Executive Room, Suite Room dengan ukuran kamar mulai dari dua puluh satu (21) meter persegi hingga tiga puluh sembilan (39) meter persegi.`,
     alamat:
-      "Jl. Jend. Soedirman No.324, Pereng, Sokanegara, Kec. Purwokerto Timur, Kabupaten Banyumas, Jawa Tengah 53116",
+      "Jl. Jend. Soedirman No.324, Pereng, Sokanegara, Kec. Purwokerto Tim., Kabupaten Banyumas, Jawa Tengah 53116",
     maps: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7030.945539579376!2d109.22849562391517!3d-7.309144998757797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6ff5d136111f1d%3A0x1067397af999a915!2sTaman%20Botani%20Baturraden!5e0!3m2!1sid!2sid!4v1699948201567!5m2!1sid!2sid",
     hotelTerdekat: [
       {
@@ -40,15 +37,17 @@ export const dataWisataById = [
     ],
     price: 500000,
     kecamatan: "Baturaden",
-    keterangan: {
-      jarak: 10,
-      buka: "08:00",
-      tutup: "17:00",
-      akomodasi: 12,
-      kolam: true,
-      parkir: true,
-      tiket: 50000,
-    },
+    fasilitas: {
+        wifi: true,
+        bar: false,
+        roomService: true,
+        breakfast: true,
+        restaurant: true,
+        pool: true,
+        parkir: true,
+        bathrom: true,
+        bedroom: true,
+      },
   },
 ];
 
@@ -57,32 +56,32 @@ export default function Page({ params }: { params: { id: string } }) {
     <div>
       <div className="flex flex-col items-center justify-center">
         <div className="flex flex-row gap-1 w-[90%] md:w-[90%] mt-10">
-          <LinkHistory link="wisata" />
+          <LinkHistory link="hotel" />
         </div>
         <h1 className="text-black text-[40px] font-semibold text-center mt-10">
-          Lokawisata {dataWisataById[0].nama}
+          {dataHotelById[0].nama}
         </h1>
 
         {/* Images */}
         <div className="flex flex-col md:flex-row w-[90%] md:w-[90%] mt-8 gap-5">
           <Image
-            src={dataWisataById[0].src.image1}
-            alt={dataWisataById[0].nama}
+            src={dataHotelById[0].src.image1}
+            alt={dataHotelById[0].nama}
             width={737}
             height={395}
             className="rounded-xl w-full md:w-[55%] shadow-sm"
           />
           <div className="flex flex-col justify-between w-full md:w-[50%] lg:w-[45%]">
             <Image
-              src={dataWisataById[0].src.image2}
-              alt={dataWisataById[0].nama}
+              src={dataHotelById[0].src.image2}
+              alt={dataHotelById[0].nama}
               width={574}
               height={187}
               className="rounded-xl w-full mb-5 lg:max-w-[507px] shadow-sm"
             />
             <Image
-              src={dataWisataById[0].src.image3}
-              alt={dataWisataById[0].nama}
+              src={dataHotelById[0].src.image3}
+              alt={dataHotelById[0].nama}
               width={574}
               height={187}
               className="rounded-xl w-full lg:max-w-[507px] shadow-sm"
@@ -92,18 +91,18 @@ export default function Page({ params }: { params: { id: string } }) {
         <div className="flex flex-col md:flex-row w-[90%] md:w-[90%] mt-8 gap-5">
           <div className="flex flex-col w-full md:w-[55%]">
             <h2 className="text-black text-3xl font-semibold">
-              Tentang Lokawisata {dataWisataById[0].nama}
+              Tentang {dataHotelById[0].nama}
             </h2>
             <p className="text-gray-500 mt-3 text-[20px] font-normal">
-              {dataWisataById[0].deskripsi}
+              {dataHotelById[0].deskripsi}
             </p>
-            <div className="grid grid-cols-2 mt-5">
-              <Keterangan detail={true} keterangan={dataWisataById[0].keterangan} />
+            <div className="grid grid-cols-2 md:grid-cols-3 mt-5">
+              <Fasilitas detail={true} fasilitas={dataHotelById[0].fasilitas} />
             </div>
           </div>
           <div className="flex flex-col w-full lg:ml-5 md:w-[50%] lg:w-[45%] shadow-md rounded-lg">
             <iframe
-              src={dataWisataById[0].maps}
+              src={dataHotelById[0].maps}
               height="333"
               className="rounded-xl"
               allowFullScreen={true}
@@ -113,7 +112,7 @@ export default function Page({ params }: { params: { id: string } }) {
             <div className="flex flex-row gap-3 my-3">
               <Icon path={mdiMapMarker} size={3} color="#FE6984" />
               <p className=" text-gray-700 text-lg font-light">
-                {dataWisataById[0].alamat}
+                {dataHotelById[0].alamat}
               </p>
             </div>
           </div>
@@ -121,35 +120,35 @@ export default function Page({ params }: { params: { id: string } }) {
 
         <div className="w-[90%] md:w-[90%] mt-10">
           <h2 className="text-black text-[25px] font-semibold mb-5">
-            Hotel terdekat wisata
+            Hotel serupa terdekat
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
-            <Link href={`/hotel/${dataWisataById[0].hotelTerdekat[0].id}`}>
+            <Link href={`/hotel/${dataHotelById[0].hotelTerdekat[0].id}`}>
               <div className="flex-1">
                 <Image
-                  src={dataWisataById[0].hotelTerdekat[0].src}
-                  alt={dataWisataById[0].hotelTerdekat[0].nama}
+                  src={dataHotelById[0].hotelTerdekat[0].src}
+                  alt={dataHotelById[0].hotelTerdekat[0].nama}
                   width={659}
                   height={390}
                   className="rounded-xl relative z-0 shadow-sm"
                 />
-                <p className="relative bottom-10 md:bottom-16 left-5 text-white text-base md:text-[25px] font-bold">
-                  {dataWisataById[0].hotelTerdekat[0].nama}
+                <p className="relative bottom-10 md:bottom-16 left-5 md:left-10 text-white text-lg md:text-[25px] font-bold">
+                  {dataHotelById[0].hotelTerdekat[0].nama}
                 </p>
               </div>
             </Link>
 
-            <Link href={`/hotel/${dataWisataById[0].hotelTerdekat[1].id}`}>
+            <Link href={`/hotel/${dataHotelById[0].hotelTerdekat[1].id}`}>
               <div className="flex-1">
                 <Image
-                  src={dataWisataById[0].hotelTerdekat[1].src}
-                  alt={dataWisataById[0].hotelTerdekat[1].nama}
+                  src={dataHotelById[0].hotelTerdekat[1].src}
+                  alt={dataHotelById[0].hotelTerdekat[1].nama}
                   width={659}
                   height={390}
                   className="rounded-xl relative z-0 shadow-sm"
                 />
-                <p className="relative bottom-10 md:bottom-16 left-5 text-white text-base md:text-[25px] font-bold">
-                  {dataWisataById[0].hotelTerdekat[1].nama}
+                <p className="relative bottom-10 md:bottom-16 left-5 md:left-10 text-white text-lg md:text-[25px] font-bold">
+                  {dataHotelById[0].hotelTerdekat[1].nama}
                 </p>
               </div>
             </Link>
@@ -157,10 +156,10 @@ export default function Page({ params }: { params: { id: string } }) {
         </div>
         <div className="w-[90%] md:w-[90%] mt-0">
           <h2 className="text-black text-[25px] font-semibold mb-5">
-            Hotel terdekat lainnya
+            Wisata terdekat lainnya
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 justify-center items-center m-auto mt-5 gap-3">
-            <CardHotel hotel={dataHotel} />
+            <CardWisata wisata={dataWisata} />
           </div>
         </div>
       </div>
--- app/ui/hotel/fasilitas.tsx
+++ app/ui/hotel/fasilitas.tsx
@@ -7,6 +7,10 @@ import {
   mdiRoomService,
   mdiFood,
   mdiFoodVariant,
+  mdiPool,
+  mdiParking,
+  mdiBathtub,
+  mdiBed,
 } from "@mdi/js";
 
 type Props = {
@@ -16,7 +20,12 @@ type Props = {
     roomService: boolean;
     breakfast: boolean;
     restaurant: boolean;
+    pool?: boolean;
+    parkir?: boolean;
+    bathrom?: boolean;
+    bedroom?: boolean;
   };
+  detail?: boolean;
 };
 
 const fasilitasHotel = [
@@ -45,28 +54,54 @@ const fasilitasHotel = [
     text: "Restaurant",
     active: (fasilitas: any) => fasilitas.restaurant,
   },
+  {
+    icon: mdiPool,
+    text: "Kolam Renang",
+    active: (fasilitas: any) => fasilitas.pool,
+  },
+  {
+    icon: mdiParking,
+    text: "Free Parkir",
+    active: (fasilitas: any) => fasilitas.parkir,
+  },
+  {
+    icon: mdiBathtub,
+    text: "Bathroom/kamar",
+    active: (fasilitas: any) => fasilitas.bathrom,
+  },
+  {
+    icon: mdiBed,
+    text: "Nyaman",
+    active: (fasilitas: any) => fasilitas.bedroom,
+  },
 ];
 
-export default function Fasilitas({ fasilitas }: Props): JSX.Element {
+export default function Fasilitas({ fasilitas, detail }: Props): JSX.Element {
   return (
     <>
-      {fasilitasHotel.map((item, index) => (
-        <div key={index} className="flex items-center mt-1">
-          <Icon
-            path={item.icon}
-            size={0.8}
-            color={item.active(fasilitas) ? "#FE6984" : "#E0E0E0"}
-            className="inline mr-2"
-          />
-          <p
-            className={clsx("text-neutral-500 text-[8px] font-light", {
-              "-": !item.active(fasilitas),
-            })}
-          >
-            {item.active(fasilitas) ? item.text : "-"}
-          </p>
-        </div>
-      ))}
+      {Object.values(fasilitas).some((value) => value) && (
+        <>
+          {fasilitasHotel.map((item, index) => (
+            <div key={index} className={clsx("flex items-center mt-1", detail && "mt-2")}>
+              <Icon
+                path={item.icon}
+                size={detail ? 1.1 : 0.8}
+                color={item.active(fasilitas) ? "#FE6984" : "#E0E0E0"}
+                className="inline mr-2"
+              />
+              <p
+                className={clsx("text-gray-500 font-light", 
+                detail ? "text-[13px] md:text-[15px]" : "text-[8px]",
+                {
+                  "-": !item.active(fasilitas),  
+                })}
+              >
+                {item.active(fasilitas) ? item.text : "-"}
+              </p>
+            </div>
+          ))}
+        </>
+      )}
     </>
   );
 }
